bee5745f7d700c09d0024811ec5c05849b27fbf7
dokdo2005 <dokdo2005@gmail.com>
2020-11-11 17:48:56 +0900

    writing area mockup complete

--- src/components/writingArea.tsx
+++ src/components/writingArea.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import '../css/writingArea.css';
 
 export default function WritingArea() {
     return(
         <div className="writingArea">
-            UserName
-            <input type="text"></input>
+            <div className="inputArea">
+                <span className="inputLabel">UserName</span>
+                <span className="inputBox"><input type="text" className="userName"></input></span>
+            </div>
+            <div className="inputArea">
+                <span className="inputLabel">Comment</span>
+                <span className="inputBox"><textarea className="userComment"></textarea></span>
+            </div>
+            <div className="buttonArea">
+                <span className="submitBtn"><button type="button">Tweet!</button></span>
+                <span className="reloadBtn"><button type="button">Check New Tweet</button></span>
+            </div>
         </div>
     )
 }
