93a0a96ec1a49d0035aae19e39ed7c0bdaf5a596
dokdo2005 <dokdo2005@gmail.com>
2020-11-16 16:19:59 +0900

    make to show messages in recent order

--- src/components/commentsArea.tsx
+++ src/components/commentsArea.tsx
@@ -10,7 +10,14 @@ interface userMsgEntry {
 }
 
 export default function CommentsArea({userMsg}: {userMsg: userMsgEntry[]}) {
-    const getUserMsg = userMsg.map(entry => (
+    const reverseUserMsg = (userMsg: userMsgEntry[]): userMsgEntry[] => {
+        let tmpArr = [];
+        for (let i = userMsg.length - 1; i >= 0; i--) {
+            tmpArr.push(userMsg[i]);
+        }
+        return tmpArr;
+    };
+    const getUserMsg = reverseUserMsg(userMsg).map(entry => (
         <CommentEntry key={entry.msgIdx} msgEntry={entry}/>
     ));
     return(
