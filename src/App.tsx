3e862daa800f0cbd2996d0089f440224efbaa9f4
dokdo2005 <dokdo2005@gmail.com>
2020-11-26 16:37:13 +0900

    add writing function

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,42 @@ import CommentsArea from './components/commentsArea';
 import dummyData from './dummyData.json';
 import './css/app.css';
 
-interface userMsgEntry {
-    msgIdx: number;
-    userName: string;
-    userMsg : string;
-    createdAt: string
-}
-
 function App() {
   const [userMsg, setUserMsg] = useState(dummyData);
-  const addUserMsg = (newMsg: userMsgEntry) => {
+  const addUserMsg = (userName: string, userComment: string) => {
     let allMsg = userMsg.slice();
+    let newMsg = {
+      msgIdx: allMsg.length + 1,
+      userName: userName,
+      userMsg: userComment,
+      createdAt: getTimeStamp(new Date())
+    }
     allMsg.push(newMsg);
     setUserMsg(allMsg);
+  };
+  const getTimeStamp = (date: Date): string => {
+    let stamp =
+     leadingZeros(date.getFullYear(), 4) + '-' +
+     leadingZeros(date.getMonth() + 1, 2) + '-' +
+     leadingZeros(date.getDate(), 2) + ' ' +
+     leadingZeros(date.getHours(), 2) + ':' +
+     leadingZeros(date.getMinutes(), 2) + ':' +
+     leadingZeros(date.getSeconds(), 2);
+    return stamp;
+  };
+  const leadingZeros = (n: number, digits: number) => {
+    let zero = '';
+    let newNumber = String(n);
+    if (newNumber.length < digits) {
+      for (let i = 0; i < digits - newNumber.length; i++)
+        zero += '0';
+    }
+    return zero + newNumber;
   };
   return (
     <div className="App">
       <TwittlerHeader/>
-      <WritingArea/>
+      <WritingArea addUserMsg={addUserMsg}/>
       <CommentsArea userMsg={userMsg}/>
     </div>
   );
--- src/components/writingArea.tsx
+++ src/components/writingArea.tsx
@@ -1,22 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../css/writingArea.css';
 
-export default function WritingArea() {
+export default function WritingArea({addUserMsg}: any) {
+    const [userComment, setUserComment] = useState({
+        name: '',
+        comment: ''
+    });
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const {name, value} = e.target;
+        setUserComment({
+          ...userComment,
+          [name]: value
+        });
+    };
+    const handleSubmit = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        e.preventDefault();
+        if (userComment.name === '') {
+            alert('사용자 이름을 입력해주세요!');
+        } else if (userComment.comment === '') {
+            alert('트윗 내용을 입력해주세요!');
+        } else {
+            addUserMsg(userComment.name, userComment.comment);
+            setUserComment({
+                name: '',
+                comment: ''
+            });
+        }
+    };
+    const handleReset = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        e.preventDefault();
+        setUserComment({
+            name: '',
+            comment: ''
+        });
+    };
     return(
         <div className="writingArea">
-            <div className="inputArea">
-                <span className="inputLabel">UserName</span>
-                <span className="inputBox"><input type="text" className="userName"></input></span>
-            </div>
-            <div className="inputArea">
-                <span className="inputLabel">Comment</span>
-                <span className="inputBox"><textarea className="userComment"></textarea></span>
-            </div>
-            <div className="buttonArea">
-                <span className="submitBtn"><button type="submit">Tweet!</button></span>
-                <span className="rewriteBtn"><button type="reset">Write Again</button></span>
-                <span className="reloadBtn"><button type="button">Check New Tweet</button></span>
-            </div>
+            <form id="writeComment">
+                <div className="inputArea">
+                    <span className="inputLabel">UserName</span>
+                    <span className="inputBox"><input type="text" name="name" className="userName" onChange={handleChange} value={userComment.name}></input></span>
+                </div>
+                <div className="inputArea">
+                    <span className="inputLabel">Comment</span>
+                    <span className="inputBox"><input type="text" name="comment" className="userComment" onChange={handleChange} value={userComment.comment}></input></span>
+                </div>
+                <div className="buttonArea">
+                    <span className="submitBtn"><button type="submit" onClick={handleSubmit}>Tweet!</button></span>
+                    <span className="rewriteBtn"><button type="reset" onClick={handleReset}>Write Again</button></span>
+                    <span className="reloadBtn"><button type="button">Check New Tweet</button></span>
+                </div>
+            </form>
         </div>
     )
 }
